7a3d4df490904f18e6823c31fa23ec58afda03ad
Ihor Kravchuk <igor@it-security.ca>
2017-07-20 23:21:59 -0400

    Updated version of the s3 buckets checker with --file option support

--- scripts.aws/aws_test_bucket.py
+++ scripts.aws/aws_test_bucket.py
@@ -6,63 +6,104 @@ import pprint
 import json
 import sys
 from optparse import OptionParser
+from termcolor import colored, cprint
 
 __author__ = "Ihor Kravchuk"
 __license__ = "GPL"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __maintainer__ = "Ihor Kravchuk"
 __email__ = "igor@it-security.ca"
 __status__ = "Development"
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+# Cheking bucket security status
+def check_bucket(bucket_name):
+    try:
+        response = s3.get_object(
+            Bucket=bucket_name,
+            Key='index.html',
+        )
+        bucket_status_code = "S3WebSite"
+    except ClientError as ex:
+    #    print ex
+    #    print ex.response
+        bucket_status_code= ex.response['Error']['Code']
+    return bucket_status_code
+
+def print_bucket_status(bucket, status):
+    if status == "S3WebSite":
+        print colored("Bucket: "+ bucket, color="yellow"), " - Found index.html, most probably S3 static web hosting is enabled"
+    elif status == "NoSuchBucket":
+        print colored("Bucket: "+ bucket, color="green"),  " - The specified bucket does not exist"
+    elif status == "AccessDenied":
+        print colored("Bucket: "+ bucket, color="blue"), " - Bucket exists, but Access Denied"
+    elif status == "NoSuchKey":
+        print colored("Bucket: "+ bucket, color="red"),  " - Bucket exists, publicly available and no S3 static web hosting, most probably misconfigured! "
+    return
 
 parser = OptionParser()
 parser.add_option('-P', '--profile', type='string', dest='aws_profile', help='Please specify AWS CLI profile')
 parser.add_option('-B', '--bucket', type='string', dest='bucket', help='Please provide bucket name')
+parser.add_option('-F', '--file', type='string', dest='file', help='Optional: file with bcuket list to check')
 
 (options, args) = parser.parse_args()
 
-#Parsing comman line input or asking user for missing information
-#Checking AWS credentials in profile
+#seting some variables
+buckets = set()
+buckets_statuses = {}
 
+#Parsing comman line input or asking user for missing information
 if options.aws_profile is None :
     parser.print_help()
     sys.exit(-1)
-elif options.bucket is None :
+elif options.bucket is None and options.file is None:
     parser.print_help()
     sys.exit(-1)
 else:
     try:
+#Checking AWS credentials in profile
         boto3.setup_default_session(profile_name=options.aws_profile)
     except:
-        print bcolors.FAIL+ "Your credentials in the profile (", options.aws_profile, " ) do not allow AWS access"+bcolors.ENDC
+        cprint("Your credentials in the profile ("+ options.aws_profile+ " ) do not allow AWS access", color="red")
         parser.print_help()
         sys.exit(-1)
 
+# Batch mode: working with files
 
+if options.file is not None:
+    content = open(options.file, "r")
+    for s3bucket in content:
+        buckets.add(s3bucket.strip())
+    content.close()
+else: buckets.add(options.bucket)
 
 
 s3=boto3.client('s3')
-try:
-    response = s3.get_object(
-        Bucket=options.bucket,
-        Key='index.html',
-    )
-    print bcolors.WARNING+ "Bucket: "+ options.bucket+ bcolors.ENDC+ " - Found index.html, most probably S3 static web hosting is enabled"
-except ClientError as ex:
-#    print ex
-#    print ex.response
-    if ex.response['Error']['Code'] == "NoSuchBucket":
-        print bcolors.OKGREEN+ "Bucket: "+ options.bucket+ bcolors.ENDC+ " - "+ ex.response['Error']['Message']
-    elif ex.response['Error']['Code'] == "AccessDenied":
-        print bcolors.OKBLUE+ "Bucket: "+ options.bucket+ bcolors.ENDC+ " - Bucket exists, but "+ ex.response['Error']['Message']
-    elif ex.response['Error']['Code'] == "NoSuchKey":
-        print bcolors.FAIL+ "Bucket: "+ options.bucket+ bcolors.ENDC+ " - Bucket exists, publicly available and no S3 static web hosting, most probably misconfigured! "
+
+# Cheking bucket statuses
+for bucket in buckets:
+    buckets_statuses[bucket] = check_bucket(bucket)
+#    print_bucket_status(bucket, buckets_statuses[bucket])
+#print buckets_statuses
+
+# Printing sorted results
+for bucket, buckets_status in sorted(buckets_statuses.iteritems(), key=lambda (k,v): (v,k)):
+    print_bucket_status(bucket, buckets_status)
+
+
+
+
+# try:
+#     response = s3.get_object(
+#         Bucket=options.bucket,
+#         Key='index.html',
+#     )
+#     print colored("Bucket: "+ options.bucket, color="yellow"), " - Found index.html, most probably S3 static web hosting is enabled"
+# except ClientError as ex:
+# #    print ex
+# #    print ex.response
+#     if ex.response['Error']['Code'] == "NoSuchBucket":
+#         print colored("Bucket: "+ options.bucket, color="green"),  " - "+ ex.response['Error']['Message']
+#     elif ex.response['Error']['Code'] == "AccessDenied":
+#         print colored("Bucket: "+ options.bucket, color="blue"), " - Bucket exists, but "+ ex.response['Error']['Message']
+#     elif ex.response['Error']['Code'] == "NoSuchKey":
+#         print colored("Bucket: "+ options.bucket, color="red"),  " - Bucket exists, publicly available and no S3 static web hosting, most probably misconfigured! "
